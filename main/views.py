33adb33ffa813185392c2089c3d5f9f8180dc7af
olegmaslov <oleg.gnome@yandex.ru>
2023-04-26 09:52:51 +0300

    Initial commit

---
+++ main/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+
+
+def index(request):
+    if request.method == 'POST':
+        name = request.POST.get('name')
+        email = request.POST.get('email')
+        message = request.POST.get('message')
+        print(f'You have new message from {name}({email}): {message}')
+    return render(request, 'main/index.html')
