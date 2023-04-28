3ee2ffb39a7671945251f7ed2462babbf9918a1c
olegmaslov <oleg.gnome@yandex.ru>
2023-04-28 13:30:03 +0300

    for fixes ORM

--- main/models.py
+++ main/models.py
@@ -1,7 +1,19 @@
 from django.db import models
 
 
+class Category(models.Model):
+    title = models.CharField(verbose_name='Заголовок')
+
+    def __str__(self):
+        return f'{self.title}'
+
+    class Meta:
+        verbose_name = 'категория'
+        verbose_name_plural = 'категории'
+
+
 class Article(models.Model):
+    # TODO: добавить ссылку на категорию
     title = models.CharField(max_length=250, verbose_name='Заголовок')
     body = models.TextField(verbose_name='Содержимое')
     preview = models.ImageField(upload_to='blog/', null=True, blank=True, verbose_name='Изображение')
