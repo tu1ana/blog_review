4dc9c9ea96286fc87721d611a63cb8f6893cb721
olegmaslov <oleg.gnome@yandex.ru>
2023-05-17 10:43:20 +0300

    fixed 5

--- main/forms.py
+++ main/forms.py
@@ -9,5 +9,8 @@ class CommentForm(forms.ModelForm):
         fields = ('comment', 'article')
 
     def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
         for field_name, field in self.fields.items():
+            if field_name == 'article':
+                field.widget = forms.HiddenInput()
             field.widget.attrs['class'] = 'form-control'
