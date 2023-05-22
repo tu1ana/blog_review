b59861adcb2d18a084a6cea099975e99494a4d0b
olegmaslov <oleg.gnome@yandex.ru>
2023-05-22 10:54:55 +0300

    fixes-6

--- users/views.py
+++ users/views.py
@@ -1,7 +1,9 @@
+from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.views import LoginView as BaseLoginView
 from django.contrib.auth.views import LogoutView as BaseLogoutView
 from django.shortcuts import render
 from django.urls import reverse_lazy
+from django.views.generic import CreateView
 
 
 class LoginView(BaseLoginView):
@@ -10,3 +12,8 @@ class LoginView(BaseLoginView):
 
 class LogoutView(BaseLogoutView):
     pass
+
+
+class RegisterView(CreateView):
+    form_class = UserCreationForm
+    template_name = 'users/login.html'
