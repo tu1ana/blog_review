71be8715d188685af4f5a3e1dd13c1acd9adcc6f
Tuyana Chirninova <hikkonki@gmail.com>
2023-09-05 13:09:28 +0300

    contact func correction

--- main/views.py
+++ main/views.py
@@ -12,5 +12,8 @@ def index(request):
 
 def contact(request):
     if request.method == 'POST':
-        print(**request.POST)
+        name = request.POST.get('name')
+        email = request.POST.get('email')
+        message = request.POST.get('message')
+        print(f'You have new message from {name}({email}): {message}')
     return render(request, 'main/contact.html')
